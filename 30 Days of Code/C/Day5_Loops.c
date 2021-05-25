4ab4f0a6bb358420b30aa60031518f3983abad8e
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-25 09:53:10 +0530

    Add files via upload

---
+++ 30 Days of Code/C/Day5_Loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    
+    for(int i=1; i<11; i++){
+        printf("%d x %d = %d\n", n, i, n*i);
+    }
+    
+    return 0;
+}
