c84ab8dd9e6063d37980ecf61194e9f5844dda19
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-27 07:55:38 +0530

    Add files via upload

---
+++ 30 Days of Code/Cpp/Day0_HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	string input_string;
+	
+	getline(cin, input_string);
+	
+	cout << "Hello, World." << endl;
+	cout << input_string;
+	
+	return 0;
+}
