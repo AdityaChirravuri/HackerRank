104f1c469df5575eed458645153b66f03c7745ca
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-28 07:43:01 +0530

    Add files via upload

---
+++ 30 Days of Code/Cpp/Day15_LinkedList.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;	
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+    public:
+
+      Node* insert(Node *head,int data)
+      {
+          //Complete this method
+          
+          Node* temp = new Node(data);
+          Node* cur = head;
+          
+          if(head == NULL)return temp;
+          else 
+            while(cur->next)cur = cur->next;
+            cur->next = temp;
+          
+          return head;
+      }
+
+      void display(Node *head)
+      {
+          Node *start=head;
+          while(start)
+          {
+              cout<<start->data<<" ";
+              start=start->next;
+          }
+      }
+};
+int main()
+{
+	Node* head=NULL;
+  	Solution mylist;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        head=mylist.insert(head,data);
+    }	
+	mylist.display(head);
+		
+}
