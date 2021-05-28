28a3d0d7421940bfcd4096b2768a6c3f865c3cb1
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-28 07:43:23 +0530

    Add files via upload

---
+++ 30 Days of Code/Cpp/Day16_Exception.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string S;
+    cin >> S;
+    try{
+        int i = stoi(S);
+        cout << i;
+    }catch(exception e){
+        cout << "Bad String";
+    }
+    return 0;
+}
