90f0529121d2185d259a2cf82529fe94d7311706
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-29 09:29:48 +0530

    Add files via upload

---
+++ 30 Days of Code/Ruby/Day23_BST_LevelOrder(BFS).rb
@@ -0,0 +1,43 @@
+class Node
+    attr_accessor :left,:right,:data
+  def initialize data
+      @left=@right=nil
+      @data=data
+    end
+end
+class Solution
+    def insert (root,data)
+        if root==nil
+            return Node.new(data)
+        else
+            if data<=root.data
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else
+                cur=self.insert(root.right,data)
+                root.right=cur
+            end
+        end
+        return root
+    end
+
+    def levelOrder(root)
+      #Write your code here
+         q = [root]
+        while !q.empty?            
+            node =  q.shift
+            q.push node.left unless node.left.nil?
+            q.push node.right  unless node.right.nil? 
+            print "#{node.data} "
+        end    
+    end
+end
+
+myTree=Solution.new
+root=nil
+T=gets.to_i
+for i in 1..T
+    data=gets.to_i
+    root=myTree.insert(root,data)
+end
+myTree.levelOrder(root)
