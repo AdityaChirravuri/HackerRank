c6d2186d5d2d8191d54ff301f04f64bf31e27ad5
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-30 11:29:20 +0530

    Add files via upload

---
+++ 30 Days of Code/Java/Day1_DataTypes.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+        /* Declare second integer, double, and String variables. */
+        int a;
+        double c;
+        String str;
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        a = scan.nextInt();
+        c = scan.nextDouble();
+        scan.nextLine();
+        str = scan.nextLine();
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+a);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(c+d);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+        System.out.println(s+str);
+        scan.close();
+	}
+}
