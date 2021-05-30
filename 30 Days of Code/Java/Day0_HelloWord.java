8c802591f975fe4c758462592cfa0176045e47f8
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-05-30 11:28:53 +0530

    Add files via upload

---
+++ 30 Days of Code/Java/Day0_HelloWord.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Solution{
+	public static void main(String []args){
+	Scanner sc = new Scanner(System.in);
+	
+	String inputString = sc.nextLine();
+	
+	sc.close();
+	
+	System.out.println("Hello, World.");
+	System.out.println(inputString);
+	}
+}
