fdf8554d1df56327306cc855355a1002a09d0a82
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-06-03 09:09:18 +0530

    Add files via upload

---
+++ 30 Days of Code/Python/Day17_MoreExceptions.py
@@ -0,0 +1,20 @@
+#Write your code here
+class Calculator:
+    def power(self, n, p):
+        if p == 0:
+            return 1
+        if n<0 or p <0 :
+            raise ValueError("n and p should be non-negative")
+        else:
+            return pow(n, p)
+        
+
+myCalculator=Calculator()
+T=int(input())
+for i in range(T):
+    n,p = map(int, input().split())
+    try:
+        ans=myCalculator.power(n,p)
+        print(ans)
+    except Exception as e:
+        print(e)   
