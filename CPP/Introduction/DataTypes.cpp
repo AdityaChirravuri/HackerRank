89655e0b49828500b7543d9ee78141d682aec7fb
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-06-10 08:55:43 +0530

    Create DataTypes.cpp

---
+++ CPP/Introduction/DataTypes.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    float b;
+    char c;
+    long d;
+    double e;
+    cin >> a >> d >> c >> b >> e;
+    printf("%d\n%ld\n%c\n%.3f\n%.9f", a, d, c, b, e);
+    
+    return 0;
+}
