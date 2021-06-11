d7bb6ab7ce791b329c185ad2ac39a52fb84340b3
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-06-11 10:02:17 +0530

    Update Intoduction_Sets.py

--- Python/Set/Intoduction_Sets.py
+++ Python/Set/Intoduction_Sets.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 def average(array):
     array = set(array)
     s = int(0)
@@ -5,3 +7,8 @@ def average(array):
     
     return s
     
+if __name__ == '__main__':
+    n = int(raw_input())
+    arr = map(int, raw_input().split())
+    result = average(arr)
+    print result
