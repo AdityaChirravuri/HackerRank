6660a2234a97f7646dbea4ddcc3f10fbc0f2a721
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-06-20 11:45:48 +0530

    Add files via upload

---
+++ ProblemSolving/Algorithms/Sorting/Correctness And Loop variant.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void insertionSort(int N, int arr[]) {
+    int i,j;
+    int value;
+    for(i=1;i<N;i++)
+    {
+        value=arr[i];
+        j=i-1;
+        while(j>=0 && value<arr[j])
+        {
+            arr[j+1]=arr[j];
+            j=j-1;
+        }
+        arr[j+1]=value;
+    }
+    for(j=0;j<N;j++)
+        printf("%d ",arr[j]);
+}
+
+int main(void) {
+
+    int N;
+    scanf("%d", &N);
+    int arr[N], i;
+    for(i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertionSort(N, arr);
+
+    return 0;
+}
