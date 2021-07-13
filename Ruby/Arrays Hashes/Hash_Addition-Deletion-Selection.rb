002b39e6f7745641384d5c4434889c494f3789e7
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-07-13 07:17:18 +0530

    Delete Hash_Addition-Deletion-Selection.rb

--- Ruby/Arrays Hashes/Hash_Addition-Deletion-Selection.rb
+++
@@ -1,7 +0,0 @@
-# Enter your code here.
-#hackerrank[543121] = 100
-hackerrank.store(543121, 100)
-
-hackerrank.keep_if{|key, values| key.is_a?(Integer)}
-
-hackerrank.delete_if{|key, values| key%2 == 0}
