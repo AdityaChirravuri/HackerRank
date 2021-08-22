09cd78aeb7210379195accf71f47a8025c8e1eb0
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-22 09:46:04 +0530

    Delete Itertools_Product.py

--- Python/IterTools/Itertools_Product.py
+++
@@ -1,9 +0,0 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import itertools as tp
-
-n = list(map(int, input().strip().split()))
-m = list(map(int, input().strip().split()))
-k = list(tp.product(n, m))
-
-for i in range(len(k)):
-    print(k[i], end=" ")
