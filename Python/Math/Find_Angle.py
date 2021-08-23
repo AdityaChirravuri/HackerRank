54e2713f7e7e9e2da35bf2ac4e92b1703bfc0419
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-23 10:26:58 +0530

    Delete Python/Math directory

--- Python/Math/Find_Angle.py
+++
@@ -1,8 +0,0 @@
-import math
-
-ab = float(input())
-bc = float(input())
-
-#d = str(math.ceil(math.degrees(math.atan(ab/bc))))
-print(str(int(round(math.degrees(math.atan(ab/bc)),0)))+'°')
-#print(d + '°')
