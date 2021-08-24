60eb7db7625ae6781ea7e878c0769ff0fe9ddcc0
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-24 07:52:18 +0530

    Delete SymmetericDif.py

--- Python/Set/SymmetericDif.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-a = set(map(int, input().split()))
-m = int(input())
-b = set(map(int, input().split()))
-d = list(a.union(b).difference(a.intersection(b)))
-d.sort()
-for i in d:
-    print(i)
