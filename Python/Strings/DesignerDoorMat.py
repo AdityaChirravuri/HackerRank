0d326030ca8b33d8888ee4a8ec55f0883ee41d79
Ch V S S Aditya <74341700+AdityaChirravuri@users.noreply.github.com>
2021-08-26 07:50:15 +0530

    Delete DesignerDoorMat.py

--- Python/Strings/DesignerDoorMat.py
+++
@@ -1,8 +0,0 @@
-#designer door mat
-
-n, m= map(int, input().split())
-#print(n, m)
-
-pattern = [('.|.'*(2*i + 1)).center(m, '-') for i in range(n//2)]
-print('\n'.join(pattern + ['WELCOME'.center(m, '-')] + pattern[::-1]))
-
